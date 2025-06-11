daf94b33e53f0a1789cf60a235bbb76cb0bab2ca
Sorbojit1 <anilsarker73@gmail.com>
2025-06-11 17:44:02 +0600

    some changes

--- factof_fact.py
+++ factof_fact.py
@@ -1,9 +1,12 @@
 import math
-def fact_of_fact(value:int):
+def fact_of_fact(value):
     result = 1
     for n in range(value,0,-1):
         fact = math.factorial(n)
         result *= fact
     return result
-
-print(fact_of_fact(2))
+try:
+    userInput = int(input('Enter an integer number \n- '))
+    print(fact_of_fact(userInput))
+except ValueError:
+    print('Integer is only allowed or the factorial is too huge.')
